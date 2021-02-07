72839b0d432c83ffdd27476472b61149e585d813
Dewa Bisma <idewagedebisma@gmail.com>
2021-02-08 01:27:31 +0800

    add a lot of thing like typed js, loader, js for change tab and content

---
+++ index.js
@@ -0,0 +1,51 @@
+// Functions
+const changeContent = (element, tabSelected) => {
+  if (element.classList.length !== 2) {
+    const activeContent = document.querySelector('.content-active');
+    const selectedContent = document.querySelector(`#${tabSelected}`);
+    const loader = document.querySelector('.bookshelf_wrapper');
+    const contentContainer = document.querySelector(
+      '.browser-content-container'
+    );
+
+    changeActiveTab(element);
+
+    activeContent.classList.remove('content-active');
+    activeContent.classList.add('content-hidden');
+
+    loader.classList.toggle('content-hidden');
+    contentContainer.style.backgroundColor = '#1d2d50';
+
+    setTimeout(() => {
+      loader.classList.toggle('content-hidden');
+      contentContainer.style.backgroundColor = null;
+
+      selectedContent.classList.add('content-active');
+      selectedContent.classList.remove('content-hidden');
+    }, 2000);
+  }
+};
+
+const changeActiveTab = (element) => {
+  const tabActive = document.querySelector('.tab-item.active');
+
+  tabActive.classList.remove('active');
+  element.classList.add('active');
+};
+
+// Typed.js
+
+const options = {
+  strings: [
+    'a Freedom En',
+    'a Freelance English Teacher',
+    'an Fully ',
+    'a Full Stack Web Developer',
+  ],
+  smartBackspace: false,
+  fadeOutDelay: 1000,
+  loop: true,
+  typeSpeed: 40,
+};
+
+const typed = new Typed('.typed', options);
