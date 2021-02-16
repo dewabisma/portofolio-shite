827fb1e428a3b3651cb54af98ccbb703f7dcd108
Dewa Bisma <idewagedebisma@gmail.com>
2021-02-16 12:37:28 +0800

    adding interaction in activities tab, fix image ration

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Data
+const activitiesHistory = [];
+
+// DOM
+const articles = document.querySelectorAll('.activities-article');
+const prevButton = document.querySelector('#previous-article');
+const nextButton = document.querySelector('#next-article');
+
 // Functions
 const changeContent = (element, tabSelected) => {
   if (element.classList.length !== 2) {
@@ -9,6 +17,12 @@ const changeContent = (element, tabSelected) => {
     );
 
     changeActiveTab(element);
+
+    if (selectedContent.id === 'activities' && activitiesHistory.length > 0) {
+      showPrevNavBtn();
+    } else {
+      disablePrevNavBtn();
+    }
 
     activeContent.classList.remove('content-active');
     activeContent.classList.add('content-hidden');
@@ -31,6 +45,47 @@ const changeActiveTab = (element) => {
 
   tabActive.classList.remove('active');
   element.classList.add('active');
+};
+
+const addHistoryToNavBtn = (prevArticle, currentArticle) => {
+  currentArticle.classList.add('content-hidden');
+  prevArticle.classList.remove('content-hidden');
+
+  activitiesHistory.pop();
+
+  disablePrevNavBtn();
+};
+
+const disablePrevNavBtn = () => {
+  prevButton.classList.add('disabled');
+};
+
+const disableNextNavBtn = () => {
+  nextButton.classList.add('disabled');
+};
+
+const showPrevNavBtn = () => {
+  prevButton.classList.remove('disabled');
+};
+
+const showNextNavBtn = () => {
+  nextButton.classList.remove('disabled');
+};
+
+const changeArticle = (element, articleSelected) => {
+  const currentArticle = document.querySelector(`.${element}`);
+  const nextArticle = document.querySelector(`.${articleSelected}`);
+
+  currentArticle.classList.add('content-hidden');
+  nextArticle.classList.remove('content-hidden');
+
+  prevButton.onclick = () => {
+    addHistoryToNavBtn(currentArticle, nextArticle);
+  };
+
+  activitiesHistory.push({ currentArticle, nextArticle });
+
+  showPrevNavBtn();
 };
 
 // Typed.js
